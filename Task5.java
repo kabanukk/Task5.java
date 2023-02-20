f5f122fdfbd6fa462ba1b90827a6c72bd8ad8fb9
kabanukk <124621011+kabanukk@users.noreply.github.com>
2023-02-20 18:37:42 +0300

    First commit

---
+++ Task5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+	    System.out.println("Ввeдитe вeс");
+        double F = console.nextInt();
+        double Gea = 9.8;
+        double Fea;
+        Fea = Gea * m;
+        System.out.println(Fea);
+        double Fmoon;
+        Fmoon = Fea/100*17;
+        System.out.println("Вeс на лунe ");
+        System.out.println(Fmoon);
+        
+	}
+}
